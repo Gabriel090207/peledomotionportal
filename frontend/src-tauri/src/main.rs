b45ab5945184f8262c6c6f58d380062e59eeb473
Gabriel Borela <gabrielborela07@gmail.com>
2026-03-07 16:56:01 -0300

    nao abre termina lwindows

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,25 +1,29 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use tauri::command;
 
 #[command]
 async fn open_profile(profile_no: String) -> Result<String, String> {
-
     let client = reqwest::Client::new();
 
     let response = client
-    .post("http://local.adspower.net:50325/api/v2/browser-profile/start")
-    .header("Authorization", "Bearer 29bc53f626a03c5702cc13b92f5940560083e482b6f6c9d6")
-    .json(&serde_json::json!({
-        "profile_no": profile_no
-    }))
+        .post("http://local.adspower.net:50325/api/v2/browser-profile/start")
+        .header(
+            "Authorization",
+            "Bearer 29bc53f626a03c5702cc13b92f5940560083e482b6f6c9d6",
+        )
+        .json(&serde_json::json!({
+            "profile_no": profile_no
+        }))
         .send()
         .await;
 
     match response {
         Ok(res) => {
-            let text = res.text().await.unwrap();
+            let text = res.text().await.unwrap_or_default();
             Ok(text)
         }
-        Err(e) => Err(e.to_string())
+        Err(e) => Err(e.to_string()),
     }
 }
 
