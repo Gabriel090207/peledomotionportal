2b015175e92dcdbe2ea947771e4c912ac7917820
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-03-10 16:06:01 -0300

    downloads api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
 from app.routes import auth
 from app.routes.ixbrowser import router as ixbrowser_router
-from fastapi.middleware.cors import CORSMiddleware
 from app.routes.test_email import router as test_email_router
 from app.routes.criar_usuario import router as criar_usuario_router
 from app.routes.kiwify_webhook import router as kiwify_router
-from fastapi.staticfiles import StaticFiles
 from app.routes.adspower import router as adspower_router
-from fastapi.responses import FileResponse
-from fastapi import HTTPException
-import os
 
 app = FastAPI()
 
@@ -18,6 +17,9 @@ app = FastAPI()
 # ------------------------------
 profiles_in_use = set()
 
+# ------------------------------
+# CORS
+# ------------------------------
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,7 +28,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# rotas
+# ------------------------------
+# ROTAS
+# ------------------------------
 app.include_router(auth.router)
 app.include_router(ixbrowser_router)
 app.include_router(test_email_router)
@@ -34,18 +38,22 @@ app.include_router(criar_usuario_router)
 app.include_router(kiwify_router)
 app.include_router(adspower_router)
 
-# pasta de downloads
-app.mount("/downloads", StaticFiles(directory="public"), name="downloads")
-
+# ------------------------------
+# ROOT
+# ------------------------------
 @app.get("/")
 def root():
     return {"status": "API online"}
 
+# ------------------------------
+# DOWNLOAD DE ARQUIVOS
+# ------------------------------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.get("/downloads/{file_name}")
 def download_file(file_name: str):
 
-    file_path = f"app/downloads/{file_name}"
+    file_path = os.path.join(BASE_DIR, "downloads", file_name)
 
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="Arquivo não encontrado")
